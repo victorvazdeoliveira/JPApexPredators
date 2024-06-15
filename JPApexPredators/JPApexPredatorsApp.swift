1e789ac49bf3cd052e81b4ba06660bee0f8caf43
Victor Vaz <triceps_mettle_0f@icloud.com>
2024-06-15 18:59:08 -0300

    Initial Commit

---
+++ JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Victor Vaz de Oliveira on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
