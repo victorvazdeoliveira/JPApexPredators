44628804e7ceeaac9eb46b0c41be318e7c3666d4
Victor de Oliveira <triceps_mettle_0f@icloud.com>
2024-06-29 23:42:21 -0300

    Atualizações nas configurações do projeto e organização do código.

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -9,16 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     let predators = Predators()
+    
     @State var searchText = ""
+    @State var alphabetical = false
     
     var filteredDinos: [ApexPredator] {
-        if searchText.isEmpty {
-            return predators.apexPredators
-        } else {
-            return predators.apexPredators.filter { predator in
-                predator.name.localizedCaseInsensitiveContains(searchText)
-            }
-        }
+        predators.sort(by: alphabetical)
+        
+        return predators.search(for: searchText)
     }
     
     var body: some View {
@@ -58,6 +56,24 @@ struct ContentView: View {
             .searchable(text: $searchText)
             .autocorrectionDisabled()
             .animation(.default, value: searchText)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        withAnimation {
+                            alphabetical.toggle()
+                        }
+                    } label: {
+//                        if alphabetical {
+//                            Image(systemName: "film")
+//                        } else {
+//                            Image(systemName: "textformat")
+//                        }
+                        
+                        Image(systemName: alphabetical ? "film" : "textformat")
+                            .symbolEffect(.bounce, value: alphabetical)
+                    }
+                }
+            }
         }
         .preferredColorScheme(.dark)
     }
