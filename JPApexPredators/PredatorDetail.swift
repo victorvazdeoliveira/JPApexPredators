b9a26b1c60f37314ac552918932afab965961408
Victor de Oliveira <triceps_mettle_0f@icloud.com>
2024-07-04 21:00:48 -0300

    Alguns detalhes do predador adicionado.

---
+++ JPApexPredators/PredatorDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredators
+//
+//  Created by Victor de Oliveira on 04/07/24.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetail()
+}
