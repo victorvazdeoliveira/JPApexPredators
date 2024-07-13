45bc59122206365c8388e2c341d896fd1412d152
Victor de Oliveira <triceps_mettle_0f@icloud.com>
2024-07-13 00:39:08 -0300

    Mapa adicionado no PredatorDetail.

--- JPApexPredators/ContentView.swift
+++ JPApexPredators/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     let predators = Predators()
@@ -26,7 +27,7 @@ struct ContentView: View {
         NavigationStack {
             List(filteredDinos) { predator in
                 NavigationLink {
-                    PredatorDetail(predator: predator)
+                    PredatorDetail(predator: predator, position: .camera(MapCamera(centerCoordinate: predator.location, distance: 30000)))
                 } label: {
                     HStack {
                         // Dinosaur image
--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct PredatorDetail: View {
     let predator: ApexPredator
+    
+    @State var position: MapCameraPosition
     
     var body: some View {
         GeometryReader { geo in
@@ -18,6 +21,13 @@ struct PredatorDetail: View {
                     Image(predator.type.rawValue)
                         .resizable()
                         .scaledToFit()
+                        .overlay {
+                            LinearGradient(stops: [
+                                Gradient.Stop(color: .clear, location: 0.8),
+                                Gradient.Stop(color: .black, location: 1)
+                            ], startPoint: .top, endPoint: .bottom)
+                        }
+                    
                     // Dino image
                     Image(predator.image)
                         .resizable()
@@ -35,10 +45,39 @@ struct PredatorDetail: View {
                         .font(.largeTitle)
                     
                     // Current location
+                    NavigationLink {
+                        
+                    } label: {
+                        Map(position: $position) {
+                            Annotation(predator.name, coordinate: predator.location) {
+                                Image(systemName: "mappin.and.ellipse")
+                                    .font(.largeTitle)
+                                    .imageScale(.large)
+                                    .symbolEffect(.pulse)
+                            }
+                            .annotationTitles(.hidden)
+                        }
+                        .frame(height: 125)
+                        .overlay(alignment: .trailing) {
+                            Image(systemName: "greaterthan")
+                                .imageScale(.large)
+                                .font(.title3)
+                                .padding(.trailing, 5)
+                        }
+                        .overlay(alignment: .topLeading) {
+                            Text("Current Location")
+                                .padding([.leading, .bottom], 5)
+                                .padding(.trailing, 8)
+                                .background(.black.opacity(0.33))
+                                .clipShape(.rect(bottomTrailingRadius: 15))
+                        }
+                        .clipShape(.rect(cornerRadius: 15))
+                    }
                     
                     // Appears in
                     Text("Appers in:")
                         .font(.title3)
+                        .padding(.top)
                     
                     ForEach(predator.movies, id: \.self) { movie in
                         Text("•" + movie)
@@ -73,10 +112,13 @@ struct PredatorDetail: View {
             }
             .ignoresSafeArea()
         }
+        .toolbarBackground(.automatic)
     }
 }
 
 #Preview {
-    PredatorDetail(predator: Predators().apexPredators[2])
-//        .preferredColorScheme(.dark)
+    NavigationStack {
+        PredatorDetail(predator: Predators().apexPredators[2], position: .camera(MapCamera(centerCoordinate: Predators().apexPredators[2].location, distance: 30000)))
+            .preferredColorScheme(.dark)
+    }
 }
