0ac0e800449439da017d892d8d54f4caafe3d7c3
Victor de Oliveira <contact@iamvictor.dev>
2024-07-20 18:55:34 -0300

    Mapa adicionado.

--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -46,7 +46,7 @@ struct PredatorDetail: View {
                     
                     // Current location
                     NavigationLink {
-                        
+                        PredatorMap(position: .camera(MapCamera(centerCoordinate: predator.location, distance: 1000, heading: 250, pitch: 80)))
                     } label: {
                         Map(position: $position) {
                             Annotation(predator.name, coordinate: predator.location) {
